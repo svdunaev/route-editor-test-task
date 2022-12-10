3f344f885cbf10e7de4b386920a527a9c8249bde
svdunaev <54572104+svdunaev@users.noreply.github.com>
2022-12-10 12:17:32 +0500

    added list and map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { YMaps, Map, Polyline } from '@pbe/react-yandex-maps';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import './App.css';
 
 function App() {
+  const [addresses, setAddresses] = useState([]);
+  const [text, setText] = useState('');
+
+  const addAddress = (evt) => {
+    if (evt.key === 'Enter' && text.trim().length) {
+      setAddresses([
+        ...addresses,
+        {
+          text,
+          id: nanoid(),
+        }
+      ])
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label>
+        <input value={text} onChange={(evt) => setText(evt.target.value)} onKeyDown={addAddress} ></input>
+      </label>
+      <ul>
+        {
+          addresses.map(a => <li key={a.id}>{a.text}</li>)
+        }
+      </ul>
+      <YMaps>
+        <div>my app with map</div>
+        <Map defaultState={{center:[55.75, 37.57], zoom: 9}} >
+          <Polyline geometry={[[55.8, 37.5], [55.8, 37.4], [55.7, 37.5], [55.7, 37.4], [55.757941, 37.607341]]} ></Polyline>
+        </Map>
+      </YMaps>
+    </>    
   );
 }
 
