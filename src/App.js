47c8e063d36aa93b691a714bffd553e47c286dae
svdunaev <54572104+svdunaev@users.noreply.github.com>
2022-12-10 18:36:31 +0500

    feat: add line connecting placemarks

--- src/App.js
+++ src/App.js
@@ -1,43 +1,98 @@
-import { YMaps, Map, Polyline } from '@pbe/react-yandex-maps';
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
-import './App.css';
+import { YMaps, Map, Polyline, Placemark } from '@pbe/react-yandex-maps'
+import { useCallback, useState } from 'react'
+import { nanoid } from 'nanoid'
+import './App.css'
+
+const API_KEY = process.env.REACT_APP_API_KEY
 
 function App() {
-  const [addresses, setAddresses] = useState([]);
-  const [text, setText] = useState('');
+  const [addresses, setAddresses] = useState([])
+  const [text, setText] = useState('')
 
-  const addAddress = (evt) => {
+  const fetchCoords = useCallback((text) => {
+    return fetch(
+      `http://geocode-maps.yandex.ru/1.x/?apikey=${API_KEY}&format=json&geocode=${text}&results=1`,
+    )
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        return data.response.GeoObjectCollection.featureMember[0].GeoObject
+          .Point.pos
+      })
+      .then((coords) => {
+        return coords.split(' ').reverse()
+      })
+  }, [])
+
+  const addAddress = async (evt) => {
     if (evt.key === 'Enter' && text.trim().length) {
+      const coords = await fetchCoords(text)
       setAddresses([
         ...addresses,
         {
           text,
           id: nanoid(),
-        }
+          coords,
+        },
       ])
     }
   }
 
+  const getGeometry = (addresses) => {
+    const geometry = []
+
+    addresses.forEach((a) => geometry.push(a.coords))
+
+    return geometry
+  }
 
   return (
     <>
       <label>
-        <input value={text} onChange={(evt) => setText(evt.target.value)} onKeyDown={addAddress} ></input>
+        <input
+          value={text}
+          onChange={(evt) => setText(evt.target.value)}
+          onKeyDown={addAddress}
+        ></input>
       </label>
       <ul>
-        {
-          addresses.map(a => <li key={a.id}>{a.text}</li>)
-        }
+        {addresses.map((address) => (
+          <li key={address.id}>{address.text}</li>
+        ))}
       </ul>
       <YMaps>
         <div>my app with map</div>
-        <Map defaultState={{center:[55.75, 37.57], zoom: 9}} >
-          <Polyline geometry={[[55.8, 37.5], [55.8, 37.4], [55.7, 37.5], [55.7, 37.4], [55.757941, 37.607341]]} ></Polyline>
+        <Map
+          width={'50%'}
+          height={'100vh'}
+          state={{ center: [55.75, 37.61], zoom: 11 }}
+        >
+          <Polyline geometry={getGeometry(addresses)}></Polyline>
+          {addresses.map((address) => (
+            <Placemark
+              key={address.id}
+              modules={['geoObject.addon.balloon']}
+              geometry={address.coords}
+              options={{ draggable: true }}
+              properties={{
+                balloonContentBody: `${address.text}`,
+              }}
+              onDragEnd={(evt) => setAddresses(prev => prev.map(a => {
+                if(a.id === address.id) {
+                  return {
+                    ...a,
+                    coords: evt.get('target').geometry.getCoordinates(),
+                  }
+                }
+                return a
+              }))}
+            />
+          ))}
         </Map>
       </YMaps>
-    </>    
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
